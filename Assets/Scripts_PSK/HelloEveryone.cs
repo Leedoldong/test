d7717cf9e12027b5bc4c2b81bed0552cbe8e7af9
Tael222 <138008560+tael222@users.noreply.github.com>
2024-06-01 02:42:59 +0900

    맥환경 업데이트 및 HelloEveryone 스크립트 생성
    맥환경 추가
    VSCode 환경 추가
    HelloEveryone 스크립트 생성 : Debug Log 출력

---
+++ Assets/Scripts_PSK/HelloEveryone.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloEveryone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Everyone! by PSK");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
